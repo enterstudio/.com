292c2fb8e5f649721d22611326ec8d14c3b79a5d
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-01-30 15:47:38 -0800

    React-Images

--- src/components/Gallery/GalleryGrid.js
+++ src/components/Gallery/GalleryGrid.js
@@ -3,6 +3,8 @@ import Img from 'gatsby-image'
 import { chunk, sum } from 'lodash'
 import { Box, Link, Heading } from 'rebass'
 
+import Carousel, { Modal, ModalGateway } from 'react-images'
+
 const Gallery = ({
   title,
   slug,
@@ -17,36 +19,72 @@ const Gallery = ({
       )
   )
 
-  return (
-    <>
-      <Box key={slug} p={[4, 5]}>
-        <Heading key={title}>{title}</Heading>
-        {images.map((image, i) => (
-          <Link key={image.id} href={image.originalImg}>
-            <Box
-              as={Img}
-              key={image}
-              fluid={image.fluid}
-              title={image.title}
-              width={rowAspectRatioSumsByBreakpoints.map(
-                (rowAspectRatioSums, j) => {
-                  const rowIndex = Math.floor(i / itemsPerRowByBreakpoints[j])
-                  const rowAspectRatioSum = rowAspectRatioSums[rowIndex]
+  class GalleryModal extends React.Component {
+    state = { modalIsOpen: false }
+    toggleModal = () => {
+      this.setState(state => ({ modalIsOpen: !state.modalIsOpen }))
+    }
+    render() {
+      const { modalIsOpen } = this.state
 
-                  return `${(image.fluid.aspectRatio / rowAspectRatioSum) *
-                    100}%`
-                }
-              )}
-              css={`
+      return (
+        <>
+          <Box key={slug} p={[4, 5]}>
+            <Heading key={title}>{title}</Heading>
+            {images.map((image, i) => (
+              <Link
+                key={image.id}
+                href={image.originalImg}
+                onClick={this.toggleModal}
+              >
+                <Box
+                  as={Img}
+                  key={image}
+                  fluid={image.fluid}
+                  title={image.title}
+                  width={rowAspectRatioSumsByBreakpoints.map(
+                    (rowAspectRatioSums, j) => {
+                      const rowIndex = Math.floor(
+                        i / itemsPerRowByBreakpoints[j]
+                      )
+                      const rowAspectRatioSum = rowAspectRatioSums[rowIndex]
+
+                      return `${(image.fluid.aspectRatio / rowAspectRatioSum) *
+                        100}%`
+                    }
+                  )}
+                  css={`
             display: inline-block;
             vertical-align: middle;
             objectFit: 'cover !important',
             height: '100%',
           `}
-            />
-          </Link>
-        ))}
-      </Box>
+                />
+              </Link>
+            ))}
+          </Box>
+
+          {ModalGateway && (
+            <ModalGateway>
+              {modalIsOpen && (
+                <Modal onClose={this.toggleModal}>
+                  <Carousel
+                    views={images.map(({ image }) => ({
+                      source: image,
+                    }))}
+                  />
+                </Modal>
+              )}
+            </ModalGateway>
+          )}
+        </>
+      )
+    }
+  }
+
+  return (
+    <>
+      <GalleryModal />
     </>
   )
 }
